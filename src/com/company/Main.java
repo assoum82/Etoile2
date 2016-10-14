e17aa9363c71299876af54ff4292eb8f8effdd5a
Mohamed-seddik <mr.bouzidaho.mohamed@gmail.com>
2016-10-14 18:09:03 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+
+package com.company;
+import java.util.Scanner;
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int space, ligne;
+        String etoile = "";
+
+        Scanner read = new Scanner(System.in);
+        
+        System.out.println("Donner le nombre de ligne du triangle ");
+        ligne = read.nextInt();
+
+        for (int i = 0; i < ligne; i++) {
+
+            space = (ligne-1) - i;
+
+            for (int j = 0; j < space; j++)
+            {
+
+                System.out.print(" ");
+
+            }
+            etoile = etoile + "* ";
+            System.out.println(etoile);
+
+        }
+    }
+}
